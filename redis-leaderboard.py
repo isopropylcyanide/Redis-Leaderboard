d625c934d2804c04db37962890c2db1dc11130b4
Aman Garg <aman.garg@accoliteindia.com>
2018-06-17 18:18:00 +0530

    Added search, remove and country leaderboard

--- redis-leaderboard.py
+++ redis-leaderboard.py
@@ -1,8 +1,16 @@
 import sys
 import redis
 
+"""
+A primitive interactive leaderboard designed in Python leveraging Redis
+Author: Aman Garg
+"""
 r = redis.StrictRedis(host='localhost', port=6379, db=0,
                       charset="utf-8", decode_responses=True)
+
+keyLeaderboard = 'leaderboard'
+keyUserPrefix = 'user_'
+keyLeaderboardCountryPrefix = 'leaderboard_country_'
 
 
 class User:
@@ -20,17 +28,24 @@ class User:
 
 def displayLeaderBoard(users):
     """Modelling a basic leaderboard with fields such as name, email, country and score"""
-    print('*'*50)
+    print('*'*55)
     for index, user in enumerate(users):
         print('%3r %10r | %10r | %10r | %10r' %
               (index + 1, user.email, user.name, user.country, user.score))
-    print('*'*50)
+    print('*'*55)
+
+
+def prettyPrint(string):
+    """Pretty print a simple string"""
+    print('*'*55)
+    print(string)
+    print('*'*55)
 
 
 COMMAND_LIST = {
     'UPSERT_USER': {
         'id': 1,
-        'args': ['name', 'email', 'country'],
+        'args': ['name', 'country', 'email'],
         'optionalArgs': []
     },
     'UPSERT_SCORE': {
@@ -54,6 +69,11 @@ COMMAND_LIST = {
         'args': [],
         'optionalArgs': ['name', 'score', 'country']
     },
+    'REMOVE_USER': {
+        'id': 5,
+        'args': ['email'],
+        'optionalArgs': []
+    }
 }
 
 
@@ -70,11 +90,14 @@ class QueryBuilder:
 
     def parseCommand(self):
         if self.command is None or not isinstance(self.command, str) or self.command not in COMMAND_LIST:
-            sys.exit('Invalid command specified. It has to be a valid string')
+            prettyPrint(
+                'Invalid command specified. It has to be a valid string')
+            return
 
         command = COMMAND_LIST[self.command]
         if len(self.inputParams[1:]) < len(command['args']):
-            sys.exit(self.command + ' is missing the required parameters')
+            prettyPrint(self.command + ' is missing the required parameters')
+            return
 
         if self.command == 'UPSERT_USER':
             name, country, email = self.inputParams[1:]
@@ -99,23 +122,29 @@ class QueryBuilder:
 
         elif self.command == 'SEARCH':
             try:
-                name = self.inputParams[2]
-            except IndentationError as e:
+                name = self.inputParams[1]
+            except IndexError:
                 name = None
             try:
-                score = self.inputParams[3]
-            except IndentationError as e:
+                score = self.inputParams[2]
+            except IndexError:
                 score = None
             try:
-                country = self.inputParams[4]
-            except IndentationError as e:
+                country = self.inputParams[3]
+            except IndexError:
                 country = None
             search(name, score, country)
 
+        elif self.command == 'REMOVE_USER':
+            email = self.inputParams[1]
+            removeUser(email)
+
 
 def addUser(name, country, email, score=0, display=True):
-    """Adds user properties by email hash to hmset and upsert score with 0"""
-    hashKey = 'user_' + email
+    """Adds user properties by email hash and upsert score with 0
+        Also add the user to the leaderboard of his country"""
+    hashKey = keyUserPrefix + email
+    country = country.capitalize()
     if r.exists(hashKey):
         print('User ' + email + ' is already present. ')
     else:
@@ -124,52 +153,108 @@ def addUser(name, country, email, score=0, display=True):
             'country': country
         }
         r.hmset(hashKey, mapping)
-        upsertScore(email, score)
+        upsertScore(email, score, country=country, display=False)
         print('User ' + email + ' added to leaderboard with a score of %s. ' % (score))
-        createLeaderboard(leaders=None, display=display)
+    createLeaderboard(leaders=None, display=display)
 
 
-def search(name, score, country):
-    pass
+def search(name=None, score=None, country=None):
+    """Search on the entire leaderboard based on the optional filters name, score, country"""
+    try:
+        leaders = None
+        if name is not None:
+            if score is not None:
+                if country is not None:
+                    # find people locally by score and name
+                    country = country.capitalize()
+                    _leaderBoardKey = keyLeaderboardCountryPrefix + country
+                    leaders = r.zrevrangebyscore(
+                        _leaderBoardKey, score, score, withscores=True)
+                    leaders = filter(lambda x: r.hget(
+                        keyUserPrefix + x[0], 'name') == name, leaders)
+                else:
+                    # find people globally with given score and name
+                    leaders = r.zrevrangebyscore(
+                        keyLeaderboard, score, score, withscores=True)
+                    leaders = filter(lambda x: r.hget(
+                        keyUserPrefix + x[0], 'name') == name, leaders)
+            else:
+                # find people globally by name
+                leaders = r.zrevrange(keyLeaderboard, 0, -1, withscores=True)
+                leaders = filter(lambda x: r.hget(
+                    keyUserPrefix + x[0], 'name') == name, leaders)
+        createLeaderboard(leaders=leaders)
+    except:
+        print('**SEARCH Usage: SEARCH [NAME] [SCORE] [COUNTRY]**')
 
 
-def upsertScore(email, score):
-    """Upsert score in a sorted set by email"""
-    r.zadd('leaderboard', score, email)
-    createLeaderboard()
+def upsertScore(email, score, country=None, display=True):
+    """Upsert score in the entire leaderboard as well as the country leaderboard"""
+    r.zadd(keyLeaderboard, score, email)
+    if country is None:
+        hashKey = keyUserPrefix + email
+        country = r.hget(hashKey, 'country')
+    country = country.capitalize()
+    r.zadd(keyLeaderboardCountryPrefix + country, score, email)
+    createLeaderboard(display=display)
+
+
+4
 
 
 def createLeaderboard(leaders=None, display=True):
     """Create the leaderboard at any given instant and print it from the given list of leaders.
         If leader list is not specified, assume the top ranings with no filters.
         Make sure all leader items carry scores"""
-    if not display:
-        return
-    if leaders is None:
-        leaders = r.zrevrange('leaderboard', 0, -1, withscores=True)
+
+    leaders = r.zrevrange(keyLeaderboard, 0, -1,
+                          withscores=True) if leaders is None else leaders
     users = []
     for leader in leaders:
         email = leader[0]
-        hashKey = 'user_' + email
+        hashKey = keyUserPrefix + email
         userDetail = r.hgetall(hashKey)
-        users.append(User(name=userDetail['name'], country=userDetail['country'],
-                          email=leader[0], score=leader[1]))
-    displayLeaderBoard(users)
+        try:
+            users.append(User(name=userDetail['name'], country=userDetail['country'],
+                              email=leader[0], score=leader[1]))
+        except KeyError:
+            print('Couldn\'t fetch attributes for %s' % (email))
+    if display:
+        displayLeaderBoard(users)
 
 
 def getTopHighest(k, country=None):
     """Get top k highest leaders. With an optional country filter"""
     if country is None:
-        leaders = r.zrevrange('leaderboard', 0, k - 1, withscores=True)
-        createLeaderboard(leaders)
+        leaders = r.zrevrange(
+            keyLeaderboard, 0, max(0, k - 1), withscores=True)
     else:
-        pass
+        country = country.capitalize()
+        leaders = r.zrevrange(keyLeaderboardCountryPrefix +
+                              country, 0, max(0, k - 1), withscores=True)
+    createLeaderboard(leaders)
 
 
 def getUsersWithScore(score):
     """Get all users with a certain score"""
-    leaders = r.zrevrangebyscore('leaderboard', score, score, withscores=True)
+    leaders = r.zrevrangebyscore(keyLeaderboard, score, score, withscores=True)
     createLeaderboard(leaders=leaders)
+
+
+def removeUser(email):
+    """Remove the user from the global leaderboard, country leaderboard 
+        and purge their details """
+    hashKey = keyUserPrefix + email
+    if not r.exists(hashKey):
+        print('User ' + email + ' doesn\'t exists ')
+    else:
+        userDetail = r.hgetall(hashKey)
+        _leaderBoardKey = keyLeaderboardCountryPrefix + userDetail['country']
+        r.zrem(keyLeaderboard, email)
+        r.zrem(_leaderBoardKey, email)
+        r.delete(hashKey)
+        print('User %s removed successfully from the global and country leaderboards' % (email))
+    createLeaderboard()
 
 
 if __name__ == "__main__":
